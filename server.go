da1326b433e36adc0eea82625e25d2d4f68e8b44
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-09 01:29:11 +0200

    Added initial server connection code - incomplete

--- server.go
+++ server.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	fmt.Println("Tick-It Card Service Starting...")
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
