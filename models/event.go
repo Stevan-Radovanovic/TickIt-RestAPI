c4a2b98098a27b3b354459be4bd83dadb912f01f
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-09 01:20:07 +0200

    Added initial version of all models

--- models/event.go
+++ models/event.go
@@ -1,1 +1,12 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+//Event Model
+type Event struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Description string             `json:"description" bson:"description,omitempty"`
+	Date        string             `json:"date" bson:"date,omitempty"`
+	Zones       []Zone             `json:"zones" bson:"zones,omitempty"`
+}
--- models/order.go
+++ models/order.go
@@ -1,1 +1,12 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+//Order Model
+type Order struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email  string             `json:"email,omitempty" bson:"email,omitempty"`
+	Ticket string             `json:"ticket" bson:"ticket,omitempty"`
+	Date   string             `json:"date" bson:"date,omitempty"`
+	Amount string             `json:"amount" bson:"amount,omitempty"`
+}
